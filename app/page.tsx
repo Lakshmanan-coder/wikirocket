54aff10b8de7a39a5a9ca3a7be61d97d4c316304
lakshmanan <lakshvj21@gmail.com>
2023-06-23 19:23:26 +0530

    Wikipedia search results app

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+
+
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <a href="#" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Please type in the search box to find results</h5>
+    <p className="font-normal text-gray-700 dark:text-gray-400">Results are list of wikipedia links related to the keyword you provided</p>
+  </a>
+    </main>
+  )
+}
